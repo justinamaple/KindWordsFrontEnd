263b855eec7aee26234c80454ac937ef09a53a3d
Kara Odle <karajo.odle@gmail.coml>
2019-11-09 14:32:15 -0800

    added initial Write component jest/enzyme test suite

--- src/components/Write.js
+++ src/components/Write.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import SendPlane from './SendPlane'
 
 class Write extends Component {
+
+  
+
   render () {
     return (
       <div className='card'>
-        <h3>This is a Write lol</h3>
-        <SendPlane />
+        <h3>This is a Write</h3>
       </div>
     )
   }
