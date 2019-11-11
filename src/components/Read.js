86748c902c63195016e15a35f1a35db35c2c95e2
Kara Odle <karajo.odle@gmail.coml>
2019-11-11 15:41:05 -0800

    changed read to functional component

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from './Button'
 import Icon from './Icon'
 
-class Read extends Component {
-  render() {
-    const { letter, handleClick, isWrite, icon } = this.props
-
-    return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <p>{letter.content}</p>
-          <span className='right floated icon'>
-            <Icon icon={letter.icon} />
-          </span>
-        </div>
-        {isWrite ? null : (
-          <Button onClick={handleClick} className='ui button'>
-            Respond to Letter
-          </Button>
-        )}
+const Read = ({ letter, handleClick, isWrite }) => {
+  return (
+    <div className='ui centered card'>
+      <div className='content'>
+        <p>{letter.content}</p>
+        <span className='right floated icon'>
+          <Icon icon={letter.icon} />
+        </span>
       </div>
-    )
-  }
+      {isWrite ? null : (
+        <Button onClick={handleClick} className='ui button'>
+          Respond to Letter
+        </Button>
+      )}
+    </div>
+  )
 }
 
 export default Read
